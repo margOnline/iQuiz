17b70fb3bce2dd02714b31a889058045dc70cf88
Margo <margo@margonline.co.uk>
2013-10-08 18:50:49 +0100

    initial commit

---
+++ iQuiz/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IQuiz::Application.config.secret_key_base = 'b70353ba60b5d19f51c199324e1d45fd6cc4a436ca58e7d4ad030cc517c361591ad39d2235c23b195b3b6fab2e9d4b7676f78b3777a06917dc4693ccb3821c26'
