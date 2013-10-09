3aa901b4e6b73b7d8b6cda77da82d9dea9501616
Margo <margo@margonline.co.uk>
2013-10-09 13:08:34 +0100

    Add iquizes index and view

--- iQuiz/app/controllers/iquizzes_controller.rb
+++ iQuiz/app/controllers/iquizzes_controller.rb
@@ -1,2 +1,7 @@
 class IquizzesController < ApplicationController
+
+
+  def index
+    @question = Question.first
+  end
 end
--- iQuiz/spec/features/question_feature_spec.rb
+++ iQuiz/spec/features/question_feature_spec.rb
@@ -2,10 +2,25 @@ require 'spec_helper'
 
 describe 'iquizzes page' do
 
+  let(:question) {FactoryGirl.create(:question, :solution_false)}
+
   it 'should display a query' do
-    FactoryGirl.create(:question, :solution_false)
+    question
     visit '/iquizzes'
     expect(page).to have_content 'Is Chicago the capital of the United States'
+  end
+
+  it 'should display a true choice' do
+    question
+    visit '/iquizzes'
+    expect(page).to have_css('#true')
+  end
+
+  it 'should display a false choice' do
+    question
+    visit '/iquizzes'
+    expect(page).to have_css('#false')
+    
   end
 
 end
