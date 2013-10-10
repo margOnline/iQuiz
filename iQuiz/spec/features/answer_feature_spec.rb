184f8389f795579ca0a204e3fc50727d1443b425
Margo <margo@margonline.co.uk>
2013-10-10 18:25:48 +0100

    Add further specs for Answer

--- iQuiz/spec/features/answer_feature_spec.rb
+++ iQuiz/spec/features/answer_feature_spec.rb
@@ -8,5 +8,13 @@ describe 'answer page' do
     visit answer_path(@answer)
     expect(page).to have_content 'Next'
   end
+
+  it 'should redirect user to home page when next link is clicked' do
+    @question = FactoryGirl.create(:question, :solution_true) 
+    @answer = FactoryGirl.create(:answer, :reply_true)
+    visit answer_path(@answer)
+    click_link("Next")
+    page.should have_content('?')
+  end
   
 end
